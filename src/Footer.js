144ff0eddde5953708666038b60a797925f22de5
rafhaelanw <rafhaelanw@gmail.com>
2024-10-04 20:20:43 +0700

    memperbaiki footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
     return (
         <div className="footer">
-            Created by Mr Sindhu
+            Created by Mr Fikky
         </div>
     )
 }
